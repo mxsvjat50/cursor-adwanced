d15047ff509080081109f724505176df65346bcd
maxsvjat50 <maxsvjatoslav@gmail.com>
2021-08-08 00:29:41 +0300

    Correction of mistakes

--- 01-HW-Hryhortsiv/main.js
+++ 01-HW-Hryhortsiv/main.js
@@ -11,17 +11,18 @@ console.log ("min price - " + minPrice);
 const totalSum= chargerPrice + phonePrice + headphonePrice;
 console.log (`total price - ${totalSum}`);
 
-const totalSumInteger = Math.floor(chargerPrice) + Math.floor(phonePrice) + Math.floor(headphonePrice)
+const totalSumInteger = Math.floor(chargerPrice) + 
+Math.floor(phonePrice) + Math.floor(headphonePrice);
 console.log (`sum integer - ${totalSumInteger}`);
 
 const totalSumRounded = Math.round(totalSumInteger/100)*100;
 console.log ("rounded sum - " + totalSumRounded);
 
 const buleanValue = (totalSumInteger) % 2;
-console.log (buleanValue == 0);
+console.log (buleanValue === 0);
 
 const customerPayment = 500;
-console.log (`change - ${customerPayment - totalSum}`)
+console.log (`change - ${customerPayment - totalSum}`);
 
 const midPrice = ((totalSum / 3).toFixed(2));
 console.log ("medium price - "+ midPrice);
@@ -30,8 +31,9 @@ const discountRandom = ((Math.random()* 100).toFixed(2));
 console.log ("customers discount - " + discountRandom + "%");
 
 const price4Customer = ((totalSum * (discountRandom / 100)).toFixed(2));
-const netProfit = (((totalSum / 2) - (totalSum - (totalSum - price4Customer))).toFixed(2))
-console.log ("net profit - " + netProfit)
+const netProfit = (((totalSum / 2) - 
+(totalSum - (totalSum - price4Customer))).toFixed(2));
+console.log ("net profit - " + netProfit);
 
 document.writeln(`
 1. Максимальна ціна:  ${maxPrice}; <br> 
@@ -39,7 +41,7 @@ document.writeln(`
 3. Сума всіх товарів:  ${totalSum}; <br> 
 4. Сума всіх товарів без копійок:  ${totalSumInteger}; <br>
 5. Сума товарів округлена до сотен:  ${totalSumRounded}; <br> 
-6. Чи є сума парним числом?  ${buleanValue == 0}; <br>
+6. Чи є сума парним числом?  ${buleanValue === 0}; <br>
 7. Решта с 500:  ${customerPayment - totalSum}; <br>
 8. Середнє значення цін: ${midPrice}; <br>
 9. Чистий прибуток: ${netProfit} `)
